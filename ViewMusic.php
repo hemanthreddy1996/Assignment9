b7e070c92a9080f23ce200ecf7eb50c6c8b5d337
hemanthreddy1996 <31603080+hemanthreddy1996@users.noreply.github.com>
2017-11-24 18:33:43 -0500

    Add files via upload

---
+++ ViewMusic.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<title>MyMusic-View</title>
+<link rel="stylesheet" type="text/css" href="./style.css" title="style" />
+</head>
+<body>
+
+<?php
+
+function show_music()
+{
+
+  $servername = "localhost";
+  $username = "hmalireddy1";
+  $password = "hmalireddy1";
+  $dbname = "hmalireddy1";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+	if ($conn->connect_error)
+	{
+
+			echo "<script type='text/javascript'>alert('Connection failed')</script>";
+	}
+
+	$sql9 = "SELECT * FROM MyMusic";
+	$result = $conn->query($sql9);
+	echo "<br><table><tr><th colspan=\"2\">MyMusic </th></tr><tr><td>Artist</td><br/><br/><td>Album</td></tr>";
+	if ($result->num_rows > 0)
+	{
+    // output data of each row
+    while($row = $result->fetch_assoc())
+	{
+        echo "<tr><td>".$row["artist"]."<td>
+		<td>".$row["album"]."<td></tr>";
+    }
+	}
+	else
+	{
+    echo "0 results";
+	}
+	echo "</table>";
+
+
+	$conn->close();
+
+}
+?>
+
+<h1><i>My Music</i></h1><br>
+
+	<h2> Music Gallery </h2>
+	<?php show_music() ?>
+<a href="./home.html"> Menu </a>
+</body>
+</html>
